09ab966191c1e2caec3ab2a1efda414b7398f75f
vince523 <xjlufe@gmail.com>
2020-09-19 13:59:33 +0800

    add: merge sort

---
+++ merge_sort.go
@@ -0,0 +1,39 @@
+package algorithm
+
+// 归并排序是一种分治策略的算法
+// 归并排序有两种递归做法：自顶向下，自底向上
+
+func MergeSort(arr []int) []int {
+	if len(arr) <= 1 {
+		return  arr
+	}
+
+	mid := len(arr) / 2
+	left := arr[:mid]
+	rigth := arr[mid:]
+	return merge(MergeSort(left), MergeSort(rigth))
+}
+
+func merge(left, right []int) []int {
+	var result []int
+	for len(left) != 0 && len(right) != 0 {
+		if left[0] <= right[0] {
+			result = append(result, left[0])
+			left = left[1:]
+		} else {
+			result = append(result, right[0])
+			right = right[1:]
+		}
+	}
+
+	for len(left) != 0 {
+		result = append(result, left[0])
+		left = left[1:]
+	}
+
+	for len(right) != 0 {
+		result = append(result, right[0])
+		right = right[1:]
+	}
+	return result
+}
---
+++ merge_sort_test.go
@@ -0,0 +1,16 @@
+package algorithm
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestMergeSort(t *testing.T) {
+	arr := []int{3, 4, 1, 6, 8, 2, 9, 7, 5}
+	res := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+	after := MergeSort(arr)
+	fmt.Println(arr)
+	fmt.Println(after)
+	assert.Equal(t, res, after)
+}
